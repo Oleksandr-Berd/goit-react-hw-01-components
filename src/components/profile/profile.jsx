f85654485e5b0f0b3531f22d0fa0cab7a48ec0fd
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-10-14 16:08:13 +0300

    1

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,59 @@
+
+export const Profile = (props) => {
+   return <div className="profile">
+  <div className="description">
+        <img
+        src={props.avatar}
+      alt="User avatar"
+      className="avatar"
+    />
+        <p className="name">{props.username}</p>
+        <p className="tag">{ props.tag}</p>
+    <p className="location">{props.location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{props.stats.followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{props.stats.views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{props.stats.likes}</span>
+    </li>
+  </ul>
+</div>
+} 
+
+
+// const rootElement = document.querySelector('#root');
+// const root = createRoot(rootElement);
+
+// root.render(
+//    <Profile
+//     username={user.username}
+//     tag={user.tag}
+//     location={user.location}
+//     avatar={user.avatar}
+//     stats={user.stats}
+//     />
+// );
+
+// App.createRoot(
+//   <Profile
+//     username={user.username}
+//     tag={user.tag}
+//     location={user.location}
+//     avatar={user.avatar}
+//     stats={user.stats}
+//   />
+// );
+
+
+
+
+
