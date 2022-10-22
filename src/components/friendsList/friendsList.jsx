bac147564a6c026961b3dc95f734d81a15c1407d
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-10-22 18:57:58 +0300

    Update friendsList.jsx

--- src/components/friendsList/friendsList.jsx
+++ src/components/friendsList/friendsList.jsx
@@ -1,20 +1,18 @@
-
 import { FriendListItem } from './friendListItem';
 
-
-export const FriendsList = (friends) => {
-  
-  const friendsArray = Object.values(friends)
-  console.log(typeof friendsArray[0][0].id);
-  return <ul>
-    {friendsArray[0].map(friend => (
-      <FriendListItem
-        id = {friend.id}
-        avatar={friend.avatar}
-        name={friend.name}
-        isOnline = {friend.isOnline}
-      />
-    )
-    )}
-  </ul>
-}
+export const FriendsList = ({ friends }) => {
+  // const friendsArray = Object.values(friends)
+  // console.log(typeof friendsArray[0][0].id);
+  return (
+    <ul>
+      {friends.map(friend => (
+        <FriendListItem
+          id={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
